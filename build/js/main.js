c7fa16f23e5a81dabe2d29f0575dd8a8dd5d145e
Ганин Александр Владимирович <87208475+PuSh0chek@users.noreply.github.com>
2022-08-08 15:20:54 +0300

    commit 5.5
    Начало адаптива под телефон

--- build/js/main.js
+++ build/js/main.js
@@ -1,1 +1,19 @@
 "use strict";
+
+const burger = document.querySelector('.navigation__wrapper-burger');
+const popUp = document.querySelector('.navigation__pop-up');
+const htmlDocument = document.querySelector('.main');
+let indexPopUp = 0;
+burger.addEventListener('click', () => {
+  if (indexPopUp === 0) {
+    popUp.style = 'display: flex';
+    htmlDocument.style = 'position: fixed';
+    indexPopUp++;
+    console.log('flex');
+  } else if (indexPopUp === 1) {
+    popUp.style = 'display: none';
+    htmlDocument.style = 'position: static';
+    indexPopUp--;
+    console.log('none');
+  }
+});
