a040ce9f39fab8b673f851fecacf1f365155e768
Ганин Александр Владимирович <87208475+PuSh0chek@users.noreply.github.com>
2022-08-08 16:24:19 +0300

    commit 5.7 link
    Настройка соббытий link

--- source/js/main.js
+++ source/js/main.js
@@ -1,18 +1,24 @@
 const burger = document.querySelector('.navigation__wrapper-burger');
 const popUp = document.querySelector('.navigation__pop-up');
-const htmlDocument = document.querySelector('.main');
+const main = document.querySelector('.main');
+const linkPopUp = document.querySelectorAll('.navigation__link');
+
 let indexPopUp = 0;
 
 burger.addEventListener('click', () => {
-  if(indexPopUp === 0) {
-    popUp.style = 'display: flex';
-    htmlDocument.style = 'position: fixed';
+  if (indexPopUp === 0) {
+    popUp.style = 'display: flex; flex-wrap: wrap;';
+    main.style = 'position: fixed';
     indexPopUp++;
     console.log('flex');
   } else if (indexPopUp === 1) {
     popUp.style = 'display: none';
-    htmlDocument.style = 'position: static';
+    main.style = 'position: static';
     indexPopUp--;
     console.log('none');
   }
+});
+
+linkPopUp.addEventListener('click', () => {
+  console.log('linkPopUp');
 });
