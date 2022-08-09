0c383c63c941b8bc174e2102ac9e4c91d4e21a86
Ганин Александр Владимирович <87208475+PuSh0chek@users.noreply.github.com>
2022-08-09 14:59:34 +0300

    5.9.3 анимация бургера

--- source/js/main.js
+++ source/js/main.js
@@ -1,13 +1,16 @@
-const burger = document.querySelector('.navigation__wrapper-burger');
-const popUp = document.querySelector('.navigation__pop-up');
+const burger = document.querySelector('.mobile-elements__wrapper-burger');
+const burgerElementUp = document.querySelector('.mobile-elements__burger-up');
+const burgerElementMiddle = document.querySelector('.mobile-elements__burger-middle');
+const burgerElementDown = document.querySelector('.mobile-elements__burger-down');
+const popUp = document.querySelector('.mobile-elements__pop-up');
 const main = document.querySelector('.main');
-const linkPopUp = document.querySelectorAll('.navigation__link');
+const linkPopUpHome = document.querySelector('.mobile-elements__link');
 
 let indexPopUp = 0;
 
 burger.addEventListener('click', () => {
   if (indexPopUp === 0) {
-    popUp.style = 'display: flex; flex-wrap: wrap;';
+    popUp.style = 'display: flex; align-items: flex-start; flex-direction: column; padding: 27px 0 0 28px';
     main.style = 'position: fixed';
     indexPopUp++;
     console.log('flex');
@@ -16,9 +19,18 @@ burger.addEventListener('click', () => {
     main.style = 'position: static';
     indexPopUp--;
     console.log('none');
+  };
+  if (indexPopUp === 1) {
+    burgerElementUp.style = 'position:absolute;transform:rotate(45deg);color:#828282;';
+    burgerElementMiddle.style = 'display:none;';
+    burgerElementDown.style = 'position:absolute;transform:rotate(-47deg);color:#828282;';
+  } else if (indexPopUp === 0) {
+    burgerElementUp.style = 'transform: rotate(0deg);color:black;';
+    burgerElementMiddle.style = 'display: block;';
+    burgerElementDown.style = 'transform: rotate(0deg);color:black;';
   }
 });
 
 linkPopUp.addEventListener('click', () => {
-  console.log('linkPopUp');
+  console.log('1');
 });
